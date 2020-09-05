f7ebec2d976c822c9d25c59e88f35b57ca1568d5
Fraser <fraser.orr@hotmail.couk>
2020-09-05 17:15:19 +0100

    Basic working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,11 @@ app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
 
-app.post('/create-readout',function(_req,res){
+app.post('/MoodObservation-readout',function(_req,res){
   
   
   
-  var mysql = require('mysql');
+ var mysql = require('mysql');
 	var config = {
 		host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
 		Database: 'birdietest',
@@ -28,10 +28,11 @@ app.post('/create-readout',function(_req,res){
   	if (err) throw err; 
   		console.log("Connected!"); 
   
-    connect.query("SELECT payload FROM birdietest.events WHERE event_type = 'mood_observation';",function (err:string, result:any){
+    connect.query("SELECT payload FROM birdietest.events WHERE event_type = 'mood_observation' order by  care_recipient_id,timestamp;",function (err:string, result:any){
       if (err) throw err;
         console.log("Query Sucess");
-        res.write("<table style='width:100%'>");
+        res.write("<table style='width:100%' border='1'>");
+        res.write("<tr><th>Mood</th><th>Additonal Notes</th><th>Date and Time</th><th>Care Recipient</th></tr>")
         result.forEach(function(value:any){
           var output = value.payload.split(',');
           
@@ -39,12 +40,46 @@ app.post('/create-readout',function(_req,res){
           //console.log(value.payload);
           console.log(output);
           //res.write("<th>"+output+"</th>");
+          var noteinrow = false;
+          
           output.forEach(function(row:any){
-            if(row.includes("mood") || row.includes("care_recipient_id") || row.includes("timestamp")  ){
-              res.write("<th>"+row+"</th>")
-            }else{
-              
+            
+            if(row.includes("note")){
+              noteinrow=true;
             }
+            if(row.includes("event")){
+             
+            }else if(row.includes("mood")|| row.includes("note") ){
+              var tmp = row.split(':');
+              var row = tmp[1];
+              var str = String(row).replace(/"/g,'');
+              
+              res.write("<th>"+str+"</th>")
+              
+            
+            }else if( row.includes("care_recipient_id") || row.includes("timestamp")  ){
+              if(noteinrow==true){
+               var tmp = row.split(':');
+               var row = tmp[1];
+               var str = String(row).replace(/"/g,'');
+               
+               res.write("<th>"+str+"</th>");
+               
+              }else{
+               var tmp = row.split(':');
+               var row = tmp[1];
+               var str = String(row).replace(/"/g,'');
+               res.write("<th>"+"</th>");
+               res.write("<th>"+str+"</th>");
+               noteinrow=true;
+              }
+              
+            
+              
+            }else{
+             
+            }
+            
             
             
           });
