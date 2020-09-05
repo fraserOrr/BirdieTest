0de35e509f4cc2bc282cc4fc83df30ce06b61ae1
Fraser <fraser.orr@hotmail.couk>
2020-09-05 19:07:01 +0100

    Basic working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
 });
 
-app.post('/MoodObservation-readout',function(_req,res){
+app.get('/MoodObservation-readout',function(_req,res){
   
   
   
@@ -101,6 +101,64 @@ app.post('/MoodObservation-readout',function(_req,res){
   //return res.redirect('http://localhost:3000/hom.html');
 });
  
+ 
+ app.get('/Concern-Raised',function(_req,res){
+  
+  var mysql = require('mysql');
+  var config = {
+   host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
+   Database: 'birdietest',
+   user: 'test-read',
+   password: 'xnxPp6QfZbCYkY8',
+   port: '3306'
+  };
+  
+  var connect = mysql.createConnection(config);
+  
+  connect.connect(function(err:string) { 
+  	if (err) throw err; 
+  		console.log("Connected!"); 
+  
+    connect.query("Select payload From birdietest.events WHERE event_type = 'concern_raised' order by care_recipient_id , timestamp;",function (err:string, result:any){
+     if (err) throw err; 
+      console.log("Query Success"); 
+      
+      res.write("<table style='width:100%' border='1'>");
+      res.write("<tr><th>note</th><th>Severity</th><th>Date and Time</th><th>Care Recipient</th></tr>");
+       // res.write("<tr><th>Mood</th><th>Additonal Notes</th><th>Date and Time</th><th>Care Recipient</th></tr>")
+      result.forEach(function(value:any){
+       var output = value.payload.split(',');
+       res.write("<tr>");
+          //console.log(value.payload);
+       console.log(output);
+       output.forEach(function(row:any){
+         if(row.includes("note")|| row.includes("severity")|| row.includes("timestamp") ||row.includes("care_recipient_id")){
+          var tmp = row.split(':');
+          var row = tmp[1];
+          var str = String(row).replace(/"/g,'');
+          res.write("<th>"+str+"</th>");
+         }else{}
+          
+          
+        });
+            
+            
+          
+        res.write("</tr>");
+        });
+        res.write("</table>");
+        res.end
+        
+         
+          
+          
+          
+    });
+  
+  //return res.redirect('http://localhost:3000/hom.html');
+  });
+ });
+  
  app.get('/',(_req,res)=> {
     res.redirect('http://localhost:3000/hom.html')
   })
